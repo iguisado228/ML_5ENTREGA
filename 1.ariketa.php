06741b0d9a0ffa2df53237feb5c09c419fc45579
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-01-25 13:16:58 +0100

    1.ariketan komentarioak gehituta

--- 1.ariketa.php
+++ 1.ariketa.php
@@ -1,15 +1,22 @@
 <?php
+//datu baseetako fitxategiari deitzen dio 
 require_once("db.php");
 
+//konexioa sortzen du datu baseekin
 $conn = konexioaSortu();
 
+//sql kontsulta bat egiten du datuak erakusteko eta postua ordenean jartzen du
 $sql = "SELECT Postua, Dortsala, Izena FROM ml_5entrega order by Postua asc";
 $result = $conn->query($sql);
 
+//taularen izenburua inprimatzen du
 echo "<h3>Pilotoen zerrenda:</h3>";
+//kontsultaren erantzunean lerroak dauden begiratzen du eta lerro guztiak inprimatzeaz zihurtatzen da
 if ($result->num_rows > 0) {
+    //taularen estiloa eta zutabeen izenburuak inprimatzen ditu
     echo "<table border='1' class='taula'>";
     echo "<tr><th>Postua</th><th>Dortsala</th><th>Izena</th></tr>";
+    //lerroak dauden bitartean lerroak inprimatzen ditu datu baseetatik hartutako informazioarekin
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["Postua"] . "</td>";
@@ -19,11 +26,14 @@ if ($result->num_rows > 0) {
     }
     echo "</table>";
 } else {
+    //taulan daturik ez badago mezu bat inprimatzen du
     echo "Ez dago daturik taulan.";
 }
+//datu baseekin konexioa isten du
 $conn->close();
 ?>
 <br>
+<!-- taula manualki inprimatzeko botoia -->
 <button class="taulaBirkargatu">Birgarkatu</button>
 
 <!DOCTYPE html>
@@ -34,6 +44,7 @@ $conn->close();
 </head>
 
 <body>
+    <!-- jquery liburutegia inportatu eta kargatzen du -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 
     <script>
@@ -41,11 +52,13 @@ $conn->close();
             // $(".taulaBirkargatu").on("click", function () {
             //     taulaBirkargatu();
             // });
+            //taulaBirkargatu funtzioa segunduro exekutatzen du taula birkargatzeko
             setInterval(taulaBirkargatu, 1000);
         });
 
+        //taula birkargatzeko funtzioa sortzen da
         function taulaBirkargatu() {
-
+            // PHP fitxategiaren URLa definitzen du eskaera prozesatzeko
             $.ajax({
                 "url": "taulaInprimatu.php",
                 "method": "GET",
@@ -53,14 +66,19 @@ $conn->close();
                     "akzioa": "taulaInprimatu",
                 }
             })
-
+                //informazioa bueltatzen duen funtzioa sortzen du
                 .done(function (bueltatutakoInfo) {
-
+                    //JSON formatuko datuak javascript eko datuetan gordetzen ditu
                     var datuak = JSON.parse(bueltatutakoInfo);
+                    //datu guztiak inprimatzeaz zihurtatzen da
                     if (datuak.kopurua > 0) {
+                        //taula hustu egiten du
                         $(".taula").html("");
+                        //taularen izenburuak inprimatzen ditu
                         $(".taula").html("<th>Postua</th><th>Dortsala</th><th>Izena</th>");
+                        //datuak hartzen ditu eta inprimatu egiten ditu lerroetan, for buklea erabiliz datu guztiak inprimatzen ditu
                         for (var i = 0; i < datuak.kopurua; i++) {
+                            //append erabiltzen da, datu bakoitza bestearen azpian inprimatzeko
                             $(".taula").append(
                                 "<tr>" +
                                 "<td>" + datuak[i].Postua + "</td>" +
@@ -69,9 +87,11 @@ $conn->close();
                                 "</tr>"
                             );
                         }
+                        //daturik ez badago mezu bat inprimatzen du
                     } else {
                         alert("Ez da elementurik kargatu");
                     }
+                    //errore bat egon bada mezu bat inprimatzen du
                 })
                 .fail(function () {
                     alert("gaizki joan da");
