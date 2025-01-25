95e9c49c5b0402938af35ba2f965c45680408f7b
iguisado228 <ira_gui_lor@goierrieskola.org>
2025-01-25 13:02:35 +0100

    datu baseak eguneratuta, id zutabea gehituta

--- ML_5ENTREGADB.sql
+++ ML_5ENTREGADB.sql
@@ -23,11 +23,12 @@ DROP TABLE IF EXISTS `ml_5entrega`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ml_5entrega` (
+  `Id` int NOT NULL AUTO_INCREMENT,
   `Postua` int DEFAULT NULL,
   `Dortsala` int NOT NULL,
   `Izena` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Dortsala`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +37,7 @@ CREATE TABLE `ml_5entrega` (
 
 LOCK TABLES `ml_5entrega` WRITE;
 /*!40000 ALTER TABLE `ml_5entrega` DISABLE KEYS */;
-INSERT INTO `ml_5entrega` VALUES (1,12,'Viñales'),(2,20,'Quartararo'),(3,26,'Pedrosa'),(9,27,'Stoner'),(4,30,'Lekuona'),(5,41,'A. Espargaró'),(8,46,'Rossi'),(6,73,'A. Marquez'),(7,93,'M. Marquez'),(10,99,'Lorenzo');
+INSERT INTO `ml_5entrega` VALUES (1,1,12,'Viñales'),(2,2,20,'Quartararo'),(3,3,26,'Pedrosa'),(4,9,27,'Stoner'),(5,4,30,'Lekuona'),(6,5,41,'A. Espargaró'),(7,8,46,'Rossi'),(8,6,73,'A. Marquez'),(9,7,93,'M. Marquez'),(10,10,99,'Lorenzo');
 /*!40000 ALTER TABLE `ml_5entrega` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-20  8:52:08
+-- Dump completed on 2025-01-25 13:01:34
--- taulaInprimatu.php
+++ taulaInprimatu.php
@@ -4,14 +4,14 @@ require_once("db.php");
 if ($_GET["akzioa"] == "taulaInprimatu") {
     $conn = konexioaSortu();
 
-    $sql = "SELECT Postua, Dortsala, Izena FROM ml_5entrega order by Postua asc";
+    $sql = "SELECT Id, Postua, Dortsala, Izena FROM ml_5entrega order by Postua asc";
     $result = $conn->query($sql);
     $pilotoak = [];
 
     if ($result->num_rows > 0) {    
         $zenbatzailea = 0;
         while ($row = $result->fetch_assoc()) {
-            $pilotoak[$zenbatzailea] = ["Postua" => $row["Postua"], "Dortsala" => $row["Dortsala"], "Izena" => $row["Izena"]];
+            $pilotoak[$zenbatzailea] = ["Id" => $row["Id"],"Postua" => $row["Postua"], "Dortsala" => $row["Dortsala"], "Izena" => $row["Izena"]];
             $zenbatzailea ++;
         }
         $pilotoak["kopurua"] = $zenbatzailea;
