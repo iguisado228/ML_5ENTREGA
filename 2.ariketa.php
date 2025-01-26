94673fba61d02245367dff2c136ef4b7140716a4
otalavera06 <oie_tal_mun@goierrieskola.org>
2025-01-26 18:09:30 +0100

    2.ariketa eta 3.ariketako iruzkinak

--- 2.ariketa.php
+++ 2.ariketa.php
@@ -1,14 +1,19 @@
 <?php
-require_once("db.php");
-$conn = konexioaSortu();
+require_once("db.php"); //datu baseekin konexioa sortzeko funtzioak dauden fitxategia deitzeko
+$conn = konexioaSortu(); //konexioa sortzen du
 
+
+ //post bidez datuak bidaltzen dira eta Id eta Postua datuak daude
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Id']) && isset($_POST['Postua'])) {
     $Id = $_POST['Id'];
     $PostuBerria = $_POST['Postua'];
 
-    $sql = "UPDATE ml_5entrega SET Postua = '$PostuBerria' WHERE Id = '$Id'";
 
-    if ($conn->query($sql) === TRUE) {
+    //datu baseetan updatea egin ahal izateko sql kontsulta
+    $sql = "UPDATE ml_5entrega SET Postua = '$PostuBerria' WHERE Id = '$Id'"; 
+
+    //sql kontsulta exekutatzen da, exekuzioa ondo joaten bada "success" mezua agertuko du
+    if ($conn->query($sql) === TRUE) { 
         echo "success"; 
     } else {
         echo "error: " . $conn->error;
@@ -17,14 +22,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['Id']) && isset($_POST[
 }
 
 
-
+//datu baseetako informazioa taulan inprimatzeko sql kontsulta
 $sql = "SELECT Id, Postua, Dortsala, Izena FROM ml_5entrega order by Postua asc";
 $result = $conn->query($sql);
 
 echo "<h3>Pilotoen zerrenda:</h3>";
+
+//taula inprimatzen da
 if ($result->num_rows > 0) {
     echo "<table border='1' class='taula'>";
     echo "<tr><th>Id</th><th>Postua</th><th>Dortsala</th><th>Izena</th></tr>";
+
+    //datu basetako linea bakoitzaren arabera informazioa agertzen du
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["Id"] . "</td>";
@@ -37,6 +46,7 @@ if ($result->num_rows > 0) {
 } else {
     echo "Ez dago daturik taulan.";
 }
+//konexioa ixten du
 $conn->close();
 ?>
 <br>
@@ -50,27 +60,38 @@ $conn->close();
 </head>
 
 <body>
+
+<!-- jquery erabili ahal izateko link-a-->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
     <script>
+        //taula birkargatzeko funtzioa
+        //documentua kargatzen denean funtzioa egingo du
         $(document).ready(function () {
             // $(".taulaBirkargatu").on("click", function () {
             //     taulaBirkargatu();
             // });
+            //honek interbalo bat neurtzen du, sartzen diogun denboraren arabera, kasu honetan taulaBirkargatu funtzioa segunduro exekutatuko du.
             setInterval(taulaBirkargatu, 1000);
         });
 
        
+    //postua eguneratzeko funtzioa function(e)-k egiten duena da botoia sakatzean ez dela orrialdea berriz kargatzen
     $("#PostuaEguneratu").submit(function (e) {
         e.preventDefault(); 
 
+        //datuak bidaltzeko formularioko datuak hartzen ditu
         let formData = $(this).serialize(); 
+        //datuak konsolan agertzen dira
         console.log("Enviando datos:", formData); 
 
+        //ajax deitzen da, datuak bidaltzeko eta jasotzeko, honetarako URL bat, tipoa eta data beharko dira
         $.ajax({
             url: "2.ariketa.php",
             type: "POST",
             data: formData,
+
+            //ondo joan bada "success" mezua agertuko da eta taula birkargatuko da
             success: function (response) {
                 console.log("Respuesta del servidor:", response); 
                 if (response.trim() === "success") {
@@ -80,6 +101,7 @@ $conn->close();
                     alert("Errorea eguneratzean: " + response); 
                 }
             },
+            //errore bat egon bada "error" mezua agertuko da
             error: function () {
                 alert("Errorea zerbitzariarekin konektatzean.");
             }
@@ -87,9 +109,9 @@ $conn->close();
     });
 
 
-
+        //taula birkargatzeko funtzioa
         function taulaBirkargatu() {
-
+            //ajax deitzen da, datuak bidaltzeko eta jasotzeko, honetarako URL bat, metodoa eta data beharko dira berriro
             $.ajax({
                 "url": "taulaInprimatu.php",
                 "method": "GET",
@@ -97,13 +119,17 @@ $conn->close();
                     "akzioa": "taulaInprimatu",
                 }
             })
-
+                
+                //behin funtzioa exekutatzen denean, datuak jasotzen dira eta taula berriz kargatzen da
                 .done(function (bueltatutakoInfo) {
 
+                    //jasotako datuak JSON formatuan daude, beraz parse egin behar da
                     var datuak = JSON.parse(bueltatutakoInfo);
+                    //jasotako datu kopurua 0 bada, alert bat agertuko da
                     if (datuak.kopurua > 0) {
                         $(".taula").html("");
                         $(".taula").html("<th>Id</th><th>Postua</th><th>Dortsala</th><th>Izena</th>");
+                        //for bat datuak taula moduan agertzeko lineaka, .append erabilita bata bestearen azpitik agertuko dira.
                         for (var i = 0; i < datuak.kopurua; i++) {
                             $(".taula").append(
                                 "<tr>" +
@@ -125,6 +151,8 @@ $conn->close();
         }
 
     </script>
+
+    <!-- Formulario bat non pilotoaren Id-a eskatzen duena eta harek izango duen postua agertzen duena-->
     <form id="PostuaEguneratu">
     <label for="Id">Sartu pilotoaren Id-a:</label>
     <input type="text" name="Id" required><br><br>
--- 3.Ariketa.php
+++ 3.Ariketa.php
@@ -8,8 +8,11 @@
 </head>
 
 <body>
+
+    <!-- Formulario bat eskualdeak aukeratzeko-->
     <form action="3.Ariketa.php" method="POST">
         <label for="eskualdea">Eskualdeak</label>
+        <!-- onchange-ak egiten du aldaketa bat gertatzean formularioan, herriakEguneratu() funtzioa abiaraziko duela-->
         <select id="eskualdea" name="eskualdea" onchange="herriakEguneratu()">
             <option value=""></option>
             <option value="Goierri">
@@ -22,14 +25,21 @@
                 Buruntzaldea
             </option>
         </select>
+        <!-- Herriak aukeratzeko formularioa-->
         <label for="herriak">Herriak</label>
         <select id="herriak" name="herriak"></select>
     </form>
+
+    <!-- jquery erabili ahal izateko link-a-->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script>
+
+        //behin orrialdea kargatzen denean, herriakEguneratu() funtzioa 1 segundoan behin exekutatuko da
         $(document).ready(function () {
             setInterval (herriakEguneratu, 1000);
         });
+
+            //herriakEguneratu() funtzioa formularioan aukeratutako eskualdearen arabera herriak aukeratzeko
             function herriakEguneratu() {
                 $.ajax({
                 "url": "3.Ariketa.php",
@@ -40,23 +50,26 @@
                 }
             })
 
+            //array bat sortu eskualdeak eta herriak gordetzeko
             const herriak = [
                 { eskualdea: 'Goierri', herriak: ['Beasain', 'Ordizia', 'Zumarraga'] },
                 { eskualdea: 'Urola', herriak: ['Zestoa', 'Azpeitia', 'Azkoitia'] },
                 { eskualdea: 'Buruntzaldea', herriak: ['Andoain', 'Urnieta', 'Lasarte-Oria']}
             ];
-                const eskualdea = document.getElementById('eskualdea').value;
-                const herriaSelect = document.getElementById('herriak');
-                herriaSelect.innerHTML = '';
-               
 
+                //eskualdearen arabera herriak aukeratzeko
+                const eskualdea = document.getElementById('eskualdea').value; //eskualdea aukeratzeko
+                const herriaSelect = document.getElementById('herriak'); //herriak aukeratzeko
+                herriaSelect.innerHTML = ''; //eskualdez aldatzerakoan, herriak ezabatzeko eta horrela beste herriak agertzeko
+               
+            
                 if (eskualdea) {
-                    const eskualdekoHerria = herriak.find(h => h.eskualdea === eskualdea);
-                    eskualdekoHerria.herriak.forEach(herriak => {
-                        const option = document.createElement('option');
+                    const eskualdekoHerria = herriak.find(h => h.eskualdea === eskualdea); //eskualdean barnean dauden herriak aukeratzeko
+                    eskualdekoHerria.herriak.forEach(herriak => { //eskualde bakoitzeko dauden herriak banaka agertzeko, hauetatik aukeratu dezazun
+                        const option = document.createElement('option'); //option elementua sortu herrien arteko aukeraketa egiteko
                         option.value = herriak;
                         option.text = herriak;
-                        herriaSelect.append(option);
+                        herriaSelect.append(option); //.append() funtzioa erabili aukeratutako herriak formularioan agertzeko bata bestearen azpian
                     });
                 }
             }
